d63b54523dd2cda5260d83384685f2150bb8c176
Luiz Felipe <85415721+LuizFelip123@users.noreply.github.com>
2022-08-06 10:28:20 -0300

    inicial commit

---
+++ src/test/java/br/edu/ifba/engenhariasoftware/Escola/EscolaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifba.engenhariasoftware.Escola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
